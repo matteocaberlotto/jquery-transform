1c5fc5ca2ddc6aab86f81f3159093d31e7e36ac1
Matteo Caberlotto <mcaber@gmail.com>
2013-09-03 19:55:06 +0200

    cosmetics

--- jquery.transform.js
+++ jquery.transform.js
@@ -1,18 +1,17 @@
 /**
  * jQuery transform
- * Applies css3 transformation using all browser properties for cross-compat.
+ * Applies css3 transformation using all browser properties for cross-compatibility.
  * author: Matteo Caberlotto
  */
-(function($) {
+(function ($) {
+    /**
+    * jquery transform
+    * author: Matteo Caberlotto
+    */
+    $.fn.transform = function (options) {
 
-	/**
-	* jquery transform
-	* author: Matteo Caberlotto
-	*/
-	$.fn.transform = function(options) {
-
-		// actual options override default ones
-		var opts = $.extend({
+        // actual options override default ones
+        var opts = $.extend({
             dx: 0,
             dy: 0,
             dz: 0,
@@ -23,32 +22,30 @@
             scaleX: 1,
             scaleY: 1,
             scaleZ: 1
-		}, options);
+        }, options);
 
-		// build the transformation string
-	    var getTransformValue = function(conf) {
-	        var transform_value = 'translate3d( ' + conf['dx'] + 'px, ' + conf['dy'] + 'px, ' + conf['dz'] + 'px )' +
-	            ' rotate3d( ' + conf['v1'] + ', ' + conf['v2'] + ', ' + conf['v3'] + ', ' + conf['angle'] + ' )' +
-	            ' scale3d( '+ conf['scaleX'] +', '+ conf['scaleY'] +', '+ conf['scaleZ'] +')';
-	        return transform_value;
-	    };
+        // build transformation string
+        var getTransformValue = function (conf) {
+            var transform_value = 'translate3d( ' + conf['dx'] + 'px, ' + conf['dy'] + 'px, ' + conf['dz'] + 'px )' +
+                ' rotate3d( ' + conf['v1'] + ', ' + conf['v2'] + ', ' + conf['v3'] + ', ' + conf['angle'] + ' )' +
+                ' scale3d( '+ conf['scaleX'] +', '+ conf['scaleY'] +', '+ conf['scaleZ'] +')';
+            return transform_value;
+        };
 
-	    // apply transformation string
-	    var assignTransformation = function (_jqueryObject, transform_value) {
-	        _jqueryObject.css({
-	              '-webkit-transform': transform_value,
-	                 '-moz-transform': transform_value,
-	                  '-ms-transform': transform_value,
-	                   '-o-transform': transform_value,
-	                      'transform': transform_value
-	        });
-	    }
+        // apply transformation string to an object
+        var assignTransformation = function (_jqueryObject, transform_value) {
+            _jqueryObject.css({
+                '-webkit-transform': transform_value,
+                   '-moz-transform': transform_value,
+                    '-ms-transform': transform_value,
+                     '-o-transform': transform_value,
+                        'transform': transform_value
+            });
+        }
 
-		// allow jquery chaining
-		return this.each(function () {
-	        assignTransformation($(this), getTransformValue(opts));
-		});
-
-	};
-
+        // allow jquery chaining
+        return this.each(function () {
+            assignTransformation($(this), getTransformValue(opts));
+        });
+    };
 })(jQuery);
